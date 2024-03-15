0aab70668581998d2d1a2371be21a231915b8cf5
CJ Lawson <clawson@mt06kw6pkw3.mynetworksettings.com>
2024-03-15 10:46:20 -0500

    client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center pt-20">
+        <div>hello</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
